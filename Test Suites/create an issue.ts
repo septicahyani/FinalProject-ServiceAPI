a69ff58285d9759f9cd641d59b3660410debc11f
kurniasepticahyani <kurnia.septi.cahyani@gmail.com>
2020-12-07 14:39:33 +0700

    final.2

---
+++ Test Suites/create an issue.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>create an issue</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>8aa2c824-2709-482c-9c2a-32d201af634c</testSuiteGuid>
+   <testCaseLink>
+      <guid>b6a641a4-9290-4993-8bef-5ad06495e64d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/create an issue covid</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>761a6bfd-caca-478a-87b1-2a038456afeb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/create an issue CNN</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
